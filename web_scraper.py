a5a49b7b49172b582aa6a251f509c868bf5f781f
Harvey Warren <51913314+Graphicks@users.noreply.github.com>
2022-07-05 11:45:33 +0100

    Added notes to make code readable

--- web_scraper.py
+++ web_scraper.py
@@ -5,42 +5,42 @@ url = 'https://www.newegg.com/global/UK-en/todays-deals?cm_sp=Head_Navigation-_-
 result = requests.get(url).text
 doc = BeautifulSoup(result, "html.parser")
 
-all_items = {}
-found_items = {}
+all_items = {} # All the items found from the initial get_items
+found_items = {} # All the items found after doing find_items
 
 class NewEgg():
 
 
-    def get_html():
+    def get_html(): # Navigates the HTML tree and returns the contents 
         app = (doc.find(id='app'))
         page_content = (app.find("div", {"class": "page-content"}))
         page_section = (page_content.find("section", {"class": "page-section is-gray-background"}))
         page_parent_div = (page_section.find("div", {"class": "page-section-inner"}))
         page_div = (page_parent_div.find("div", {"class": "item-cells-wrap tile-cells five-cells"}))
 
-        page_div_contents = (page_div.contents)
+        page_div_contents = (page_div.contents) 
 
         return page_div_contents
 
-    def get_items(page_div_contents):
+    def get_items(page_div_contents): # Looks through the contents to find the title and price of the product
         for table_rows in page_div_contents[:30]:
             title = (table_rows.a.img['title'])
-            price = (f"{table_rows.strong.string}{table_rows.sup.string}").replace(",", '')
-            all_items[title] = price
+            price = (f"{table_rows.strong.string}{table_rows.sup.string}").replace(",", '') # Adds the strong price to the sup price and removes any commas, this allows us to convert into a float.
+            all_items[title] = price # Stores the name and the price in a dictionary
 
         return all_items
     
-    def find_items(all_items, item="", budget=0):
+    def find_items(all_items, item="", budget=0): # Looks through the items we got and finds deals based off the users desires
         for i in range(len(all_items)):
-            item_price = (list(all_items.items()))[i][1]
-            item_name = (list(all_items.keys()))[i]
+            item_price = (list(all_items.items()))[i][1] # The current price of the item we're looking at
+            item_name = (list(all_items.keys()))[i] # The current name of the item we're looking at 
                 
-            if budget >= float(item_price)  and item in item_name: 
+            if budget >= float(item_price)  and item in item_name: # If the budget is greater than the price of the item and the item is what we specified it will put that deal into a dictionary
                 for y in range(len(all_items)):
                     found_items[item_name] = item_price
                 
                                                 
-            elif item == "" and budget == 0:
+            elif item == "" and budget == 0: # If the user does not input a budget or item it will return all items in a dictionary 
                 for y in range(len(all_items)):
                     found_items[item_name] = item_price
         return (found_items)
