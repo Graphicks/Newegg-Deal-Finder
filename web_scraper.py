da518ba47ab31d7728d2d47a8b889847f4f458d7
Harvey Warren <51913314+Graphicks@users.noreply.github.com>
2022-07-05 10:17:10 +0100

    Web Scraper
    Beta Web Scraper.

---
+++ web_scraper.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+
+# User Inputs
+item = input("What item are you looking for?    ")
+budget = int(input("What is your budget?    "))
+
+
+url = 'https://www.newegg.com/global/UK-en/todays-deals?cm_sp=Head_Navigation-_-Under_Search_Bar-_-Today%27s+Best+Deals&icid=650439'
+result = requests.get(url).text
+doc = BeautifulSoup(result, "html.parser")
+
+fixed_prices = {}
+
+def find_item(item="", budget=0):
+    app = (doc.find(id='app'))
+    page_content = (app.find("div", {"class": "page-content"}))
+    page_section = (page_content.find("section", {"class": "page-section is-gray-background"}))
+    page_parent_div = (page_section.find("div", {"class": "page-section-inner"}))
+    page_div = (page_parent_div.find("div", {"class": "item-cells-wrap tile-cells five-cells"}))
+
+    page_div_contents = (page_div.contents)
+    
+
+
+    for table_rows in page_div_contents[:30]:
+        title = (table_rows.a.img['title'])
+        price = (f"{table_rows.strong.string}{table_rows.sup.string}").replace(",", '')
+        fixed_prices[title] = price
+
+
+
+    for i in range (len(fixed_prices)):
+        item_price = (list(fixed_prices.items()))[i][1]
+        item_name = (list(fixed_prices.keys()))[i]
+        if float(item_price) <= budget and item in item_name: 
+            print(f"\nFound {item_name} for £{item_price} on Newegg!\n") 
+        elif item == "" and budget == 0:
+            print(item_name, item_price)
+
+find_item(item, budget)
